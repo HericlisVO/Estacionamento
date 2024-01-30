518a38cff3c0e9ed6e4d2baecaffc85f51452e78
Hericlis Ventura <hericlisventura.o@gmail.com>
2024-01-30 09:17:39 -0300

    Criando a classe do Pacote Utilitario

---
+++ src/impacta/ead/estacionamento/utilitario/EstacionamentoUtil.java
@@ -0,0 +1,41 @@
+package impacta.ead.estacionamento.utilitario;
+
+import impacta.ead.estacionamento.negocio.Movimentacao;
+
+/*
+ * Representa uma classe de apoio ás demais do sistema.
+ * 
+ * @Author Hericlis Ventura*/
+public class EstacionamentoUtil {
+
+	/*
+	 * Valida placa com padrão nacional LLL-NNNN L= Letra N = Numero
+	 * 
+	 * 
+	 * @param placa Placa do Veiculo
+	 * 
+	 * @return true se atender o padrão e false senão
+	 */
+	public boolean validarPadraoPadrao() {
+		// TODO implementar
+		return false;
+	}
+
+
+	/*
+	 *O calculo do valorda estada do veiculo baseado no tarifario
+	 *e na hora da entrada e saida do veiculo
+	 * 
+	 * 
+	 * Altera a própria instancia do parametro
+	 * 
+	 * @param movimentacao Instancia da /movimentacao
+	 * 
+	 * 
+	 * @return true se atender o padrão e false senão
+	 */
+	public void calcularPagamento(Movimentacao movimentacao) {
+		// TODO implementar
+	}
+
+}
